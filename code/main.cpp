24a0e98db5a5a9b25822dce34dfa2965aee90c02
Jordy Alkema <me@jordyalkema.nl>
2019-04-02 13:16:37 +0200

    Found fix for ADC and is now functional

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,40 @@
 #include "hwlib.hpp"
 
+
+// Voltage * 1000
+#define battery_max_voltage 12600
+#define battery_empty_voltage 10800
+
+#define voltage_meter_deviation -2500
+
+
 int main(void) {
 
-    auto batteryVoltagePin = hwlib::target::pin_in(hwlib::target::pins::a1);
-    double currentVoltage = 0.00;
-
-  // kill the watchdog
+    // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
+    namespace target = hwlib::target;
+
+    auto batteryVoltagePin = hwlib::target::pin_adc(target::ad_pins::a1);
+    int onePercent = (battery_max_voltage - battery_empty_voltage) / 100;
+
+    int currentVoltage = battery_max_voltage;
+    int currentPercentage = 100;
+
     for (;;){
         // Multiply by 5 due to the voltage sensor dividing the voltage by 5 so it is readable by the Arduino Analog pin.
-        currentVoltage = batteryVoltagePin.read() * 5;
+        currentVoltage = (batteryVoltagePin.read() * 5) + voltage_meter_deviation;
 
-        hwlib::cout << "Battery V:" << int(currentVoltage) << hwlib::endl;
+        // If battery voltage is below empty, battery percentage is 0
+        if (currentVoltage <= battery_empty_voltage){
+            currentPercentage = 0;
+        }else{
+            currentPercentage = (currentVoltage - battery_empty_voltage) / onePercent;
+        }
+
+        hwlib::cout << "Battery V:" << currentVoltage << hwlib::endl;
+        hwlib::cout << "Battery percentage:" << currentPercentage << hwlib::endl;
+
         hwlib::wait_ms(250);
     }
 }
